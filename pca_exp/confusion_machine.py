c3fb56d53a6ed18b36fa4a376c6c5fe355bc0c29
TymoteuszTula <tymoteusz.tula@gmail.com>
2021-03-22 21:04:15 +0100

    added confusion_machine.py

---
+++ pca_exp/confusion_machine.py
@@ -0,0 +1,11 @@
+# confusion_machine.py
+
+import tensorflow as tf
+
+class ConfusionMachine:
+
+    def __init__(self, range, neuron_vector, activation_vector):
+        pass
+
+    def perform_confusion_on_pcs(self, pca_machine, up_to):
+        pass
